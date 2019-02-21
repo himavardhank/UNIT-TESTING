785a8a4c86d72c18a118714adcde844e97195e05
himavardhank <himavardhank@gmail.com>
2019-02-21 12:41:48 +0530

    for unittestfailure

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ import unittest
 class Testcase(unittest.TestCase):
         def setUp(self):
                 print('setup method execution')
-        def test(self):
-                print('test method execution')
-				print(10/0)#for testing of unit testfailre scenario
+        def test_method1(self):
+                print('test method1 execution')
+        def test_method2(self):
+                print('test method2 execution')
         def tearDown(self):
-                print('teardown method execution')
+                print('teardown')
 unittest.main()
+
+#for using different test methods
